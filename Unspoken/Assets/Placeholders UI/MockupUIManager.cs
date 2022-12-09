fe8d9abbd04638000f7b085777c08f5f87d05f9b
alvaropazActrio <martinpazmino13@gmail.com>
2022-12-09 17:27:19 +0100

    UI Mockup (Placeholders)
    Backbone of UI Mockup finished.
    Next steps:
    - Animation of Bottle opening
    - AR Throwing bottles

---
+++ Unspoken/Assets/Placeholders UI/MockupUIManager.cs
@@ -0,0 +1,105 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Niantic.ARDK;
+using Niantic.ARDK.AR.Configuration;
+using Niantic.ARDK.Utilities.Input.Legacy;
+using Niantic.ARDK.AR;
+using System;
+using UnityEngine.Events;
+
+public class MockupUIManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject welcomeCanvas;
+
+    [SerializeField]
+    GameObject scanningCanvas;
+    /*
+    [SerializeField]
+    GameObject mainGameCanvas;
+    */
+    private CanvasGroup welcomeCanvasGroup;
+    private CanvasGroup scanningCanvasGroup;
+
+    public UnityEvent OnFirstTouch;
+    public UnityEvent OnSecondTouch;
+    public UnityEvent OnThirdTouch;
+    //UnityEvent OnForthTouch;
+
+    int touchCount = 0;
+    bool welcome = true;
+    bool scanning = false;
+    private void Awake()
+    {
+        welcomeCanvasGroup = welcomeCanvas.GetComponent<CanvasGroup>();
+        scanningCanvasGroup = scanningCanvas.GetComponent<CanvasGroup>();
+
+        if (OnFirstTouch == null) OnFirstTouch = new UnityEvent();
+        OnFirstTouch.AddListener(TurnOffWelcomeCanvas);
+        OnFirstTouch.AddListener(TurnOnScanningCanvas);
+
+        if (OnSecondTouch == null) OnSecondTouch = new UnityEvent();
+
+
+        if (OnThirdTouch == null) OnThirdTouch = new UnityEvent();
+
+        //if (OnForthTouch == null) OnForthTouch = new UnityEvent();
+        //OnForthTouch.AddListener(Reset);
+    }
+
+    private void TurnOffWelcomeCanvas()
+    {
+        welcome = false;
+    }
+
+    private void TurnOnScanningCanvas()
+    {
+        scanning = true;
+    }
+
+    private void Update()
+    {
+        checkTouch();
+
+        if (!welcome)
+        {
+            welcomeCanvasGroup.alpha -= Time.deltaTime;
+        }
+        if (scanning && welcomeCanvasGroup.alpha == 0)
+        {
+            scanningCanvasGroup.alpha += Time.deltaTime;
+        }
+    }
+
+    void checkTouch()
+    {
+        if (PlatformAgnosticInput.touchCount > 0)
+        {
+            var touch = PlatformAgnosticInput.GetTouch(0);
+            if (touch.phase != TouchPhase.Began) return;
+
+            touchCount++;
+
+
+            switch (touchCount)
+            {
+                case 0:
+                    Debug.Log("Que");
+                    break;
+                case 1:
+                    OnFirstTouch.Invoke();
+                    break;
+                case 2:
+                    OnSecondTouch.Invoke();
+                    break;
+                case 3:
+                    OnThirdTouch.Invoke();
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
